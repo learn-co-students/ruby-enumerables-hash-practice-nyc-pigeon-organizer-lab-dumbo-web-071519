f8fb70ae1289a109beb275fdf3b81f9b3d5f4703
Janet Mendez <janetmendez.03@gmail.com>
2019-07-03 05:41:56 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,26 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.keys.reduce({}) {|memo, category|
+  
+    data[category].each{|(key, val)|
+    
+      val.each{|pigeon_name|
+      
+        ### if the pigeon doesn't have an existing entry, add it
+        if !memo.include?(pigeon_name)
+          memo[pigeon_name] = { category => ["#{key.to_s}"] }
+        else
+          
+          ### push new value in existing category entry
+          if memo[pigeon_name][category]
+            memo[pigeon_name][category].push("#{key.to_s}")
+          ### else, create a new entry with the new value
+          else 
+             memo[pigeon_name][category] = ["#{key.to_s}"]
+          end
+          
+        end
+      }
+    }
+    p memo
+  }
 end
