f9f1b82e07005782bbc70a30fef5cd9b92915f57
Alessandra Vertrees <avertrees447@gmail.com>
2019-07-07 16:27:46 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,31 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeons = {}
+  data.each do |key, value| 
+    #puts "this is a key: #{key}. "
+    #puts "this is a value: #{value}"
+    value.each do |attribute, list|
+      #puts "this #{key} is #{attribute}"
+      list.each do |name|
+        #puts "pigeons name is #{name}"
+        if pigeons.has_key? name 
+          puts "#{name} is in hash"
+          if pigeons[name].has_key? key
+            pigeons[name][key] << attribute.to_s
+          else
+            pigeons[name][key] = []
+            pigeons[name][key] << attribute.to_s
+          end
+        else 
+          puts "#{name} is not in hash"
+          pigeons[name] = {}
+          pigeons[name][key] = []
+          pigeons[name][key] << attribute.to_s
+        end
+      end
+    end
+  end
+  puts pigeons
+  
+  return pigeons
 end
