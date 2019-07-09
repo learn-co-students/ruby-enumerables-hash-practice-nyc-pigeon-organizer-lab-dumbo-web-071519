7a7424029e8928d49b731f7082e4df6167776220
Matthew Guzman <tomaffew@me.com>
2019-07-09 06:10:46 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+    data.each{|key_atr, value_atr| 
+    value_atr.each{|key_param, value_param| 
+    value_param.each{|i|
+      if !new_hash[i]
+        new_hash[i] = {key_atr => [key_param.to_s]}
+      elsif new_hash[i][key_atr]
+        new_hash[i][key_atr].push(key_param.to_s)
+      elsif !new_hash[i][key_atr]
+        new_hash[i][key_atr] = [key_param.to_s]
+      end
+      }
+     }
+    }
+new_hash
 end
